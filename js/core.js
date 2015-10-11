8108b0f2a1cf6cc4b538722ea0fc47d16761d7c7
Joy Neop <joyneop@gmail.com>
2015-10-11 04:40:37 +0000

    bug fix: unknown

--- js/core.js
+++ js/core.js
@@ -10,7 +10,7 @@ window.enat = {
 
 enat.listOfDicts = [
     { id: 'test', title: 'Philosophy', count: 4, style: 'background-color: #19F;', color: '#19F', disabled: false },
-    { id: 'test2', title: 'Linguistics', count: 10, style: 'background-color: #1E6;', color: '#1E6', disabled: true },
+    { id: 'test2', title: 'Linguistics', count: 0, style: 'background-color: #1E6;', color: '#1E6', disabled: true },
     { id: 'test3', title: 'Math', count: 0, style: 'background-color: #FF5E3A;', color: '#FF5E3A', disabled: true },
     { id: 'test4', title: 'Physics', count: 0, style: 'background-color: #4C55A6;', color: '#4C55A6', disabled: true },
     { id: 'test5', title: 'Chemistry', count: 0, style: 'background-color: #777;', color: '#777', disabled: true },
